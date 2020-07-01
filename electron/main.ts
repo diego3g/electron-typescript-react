20ad4b704c3f63735f2288a135c122e7033ce0b4
Lucas Campelo <campelo.lucas@hotmail.com>
2020-07-01 20:38:02 -0300

    Including React Developer Tools and Redux Devtools

--- electron/main.ts
+++ electron/main.ts
@@ -1,6 +1,7 @@
 import { app, BrowserWindow } from 'electron'
 import * as path from 'path'
 import * as url from 'url'
+import installExtension, { REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } from 'electron-devtools-installer'
 
 let mainWindow: Electron.BrowserWindow | null
 
@@ -32,4 +33,15 @@ function createWindow () {
 }
 
 app.on('ready', createWindow)
+  .whenReady()
+  .then(() => {
+    if (process.env.NODE_ENV === 'development') {
+      installExtension(REACT_DEVELOPER_TOOLS)
+          .then((name) => console.log(`Added Extension:  ${name}`))
+          .catch((err) => console.log('An error occurred: ', err));
+      installExtension(REDUX_DEVTOOLS)
+          .then((name) => console.log(`Added Extension:  ${name}`))
+          .catch((err) => console.log('An error occurred: ', err));
+    }
+  })
 app.allowRendererProcessReuse = true
