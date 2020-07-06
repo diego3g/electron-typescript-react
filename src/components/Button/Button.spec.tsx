6c4714e783840f7addc51669cf40a4c6e7063e34
thomasJs8 <thomas18thx@gmail.com>
2020-07-06 12:39:48 +0900

    test(setuptests.ts): created setupTests.ts to use jest-dom globally
    moved to the extend from jest to a global setup file

--- src/components/Button/Button.spec.tsx
+++ src/components/Button/Button.spec.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
-
 import Button from './index'
 
 test('button should renders', () => {
