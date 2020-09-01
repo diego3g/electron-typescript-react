61264a57968ddd214b57e516a8a6df16304f40ce
Blair Wilcox <reccanti@gmail.com>
2020-08-31 20:42:44 -0400

    chore: update packages
    Updates packages to remove security vulnerabilities and fix the way extensions were installed

--- electron/main.ts
+++ electron/main.ts
@@ -1,11 +1,14 @@
 import { app, BrowserWindow } from 'electron'
 import * as path from 'path'
 import * as url from 'url'
-import installExtension, { REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } from 'electron-devtools-installer'
+import installExtension, {
+  REACT_DEVELOPER_TOOLS,
+  REDUX_DEVTOOLS
+} from 'electron-devtools-installer'
 
 let mainWindow: Electron.BrowserWindow | null
 
-function createWindow() {
+function createWindow () {
   mainWindow = new BrowserWindow({
     width: 1100,
     height: 700,
@@ -32,16 +35,17 @@ function createWindow() {
   })
 }
 
-app.on('ready', createWindow)
+app
+  .on('ready', createWindow)
   .whenReady()
   .then(() => {
     if (process.env.NODE_ENV === 'development') {
       installExtension(REACT_DEVELOPER_TOOLS)
         .then((name) => console.log(`Added Extension:  ${name}`))
-        .catch((err) => console.log('An error occurred: ', err));
+        .catch((err) => console.log('An error occurred: ', err))
       installExtension(REDUX_DEVTOOLS)
         .then((name) => console.log(`Added Extension:  ${name}`))
-        .catch((err) => console.log('An error occurred: ', err));
+        .catch((err) => console.log('An error occurred: ', err))
     }
   })
 app.allowRendererProcessReuse = true
