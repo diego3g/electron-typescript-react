085f460a7219c9eca3d049687410eb04953c2722
Blair Wilcox <reccanti@gmail.com>
2020-09-05 02:21:11 -0400

    fix(bundling): change how bundles are created
    Changed how the "main" electron process is bundled. The webpack config was causing issues becuase it
    would try to put things in a "browser" context. This just uses a simple "tsc" method that transpiles
    the files but otherwise leaves them as-is

--- electron/main.ts
+++ electron/main.ts
@@ -1,50 +1,56 @@
-import { app, BrowserWindow } from 'electron'
-import * as path from 'path'
-import * as url from 'url'
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+import * as url from 'url';
 import installExtension, {
   REACT_DEVELOPER_TOOLS,
-  REDUX_DEVTOOLS
-} from 'electron-devtools-installer'
+  REDUX_DEVTOOLS,
+} from 'electron-devtools-installer';
+import { setupMainListener } from './core/mainBotListener';
 
-let mainWindow: Electron.BrowserWindow | null
+let mainWindow: Electron.BrowserWindow | null;
 
-function createWindow () {
+function createWindow() {
   mainWindow = new BrowserWindow({
     width: 600,
     height: 800,
     webPreferences: {
-      nodeIntegration: true
-    }
-  })
+      nodeIntegration: true,
+    },
+  });
 
+  console.log(process.env.NODE_ENV);
   if (process.env.NODE_ENV === 'development') {
-    mainWindow.loadURL('http://localhost:4000')
+    console.log('creating window');
+    mainWindow.loadURL('http://localhost:4000');
   } else {
     mainWindow.loadURL(
       url.format({
-        pathname: path.join(__dirname, 'renderer/index.html'),
+        pathname: path.resolve(__dirname, '../dist-renderer/index.html'),
         protocol: 'file:',
-        slashes: true
+        slashes: true,
       })
-    )
+    );
   }
 
   mainWindow.on('closed', () => {
-    mainWindow = null
-  })
+    mainWindow = null;
+  });
 }
 
 app
-  .on('ready', createWindow)
+  .on('ready', () => {
+    setupMainListener();
+    createWindow();
+  })
   .whenReady()
   .then(() => {
     if (process.env.NODE_ENV === 'development') {
       installExtension(REACT_DEVELOPER_TOOLS)
         .then((name) => console.log(`Added Extension:  ${name}`))
-        .catch((err) => console.log('An error occurred: ', err))
+        .catch((err) => console.log('An error occurred: ', err));
       installExtension(REDUX_DEVTOOLS)
         .then((name) => console.log(`Added Extension:  ${name}`))
-        .catch((err) => console.log('An error occurred: ', err))
+        .catch((err) => console.log('An error occurred: ', err));
     }
-  })
-app.allowRendererProcessReuse = true
+  });
+app.allowRendererProcessReuse = true;
