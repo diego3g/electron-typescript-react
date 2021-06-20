af387aaccecada494453f64073a7413d024617f4
Diego Fernandes <diego.schell.f@gmail.com>
2021-06-20 17:37:58 -0300

    chore: Add comment to assets path

--- electron/main.ts
+++ electron/main.ts
@@ -5,8 +5,14 @@ let mainWindow: BrowserWindow | null
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string
 declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string
 
+// const assetsPath =
+//   process.env.NODE_ENV === 'production'
+//     ? process.resourcesPath
+//     : app.getAppPath()
+
 function createWindow () {
   mainWindow = new BrowserWindow({
+    // icon: path.join(assetsPath, 'assets', 'icon.png'),
     width: 1100,
     height: 700,
     backgroundColor: '#191622',
