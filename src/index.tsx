8d7796a49c65c3dc312d1b5f7e5d29943181f760
NorthBei <seekingfor520@gmail.com>
2022-07-24 16:38:55 +0800

    Upgraded all package and updated react render api, electron-forge config

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,10 @@
-import ReactDOM from "react-dom";
-import { App } from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { App } from './App'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root') || new HTMLBodyElement())
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
